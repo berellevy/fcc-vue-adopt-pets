527cef9304e24a47067ee010fe43612b089cf1b5
berellevy <berellevy@gmail.com>
2020-10-13 10:56:39 -0400

    add index and show for dogs and cats

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    Pet
+    <h1>{{ animal.name }} ({{ $route.params.species }}) </h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }} </p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const { species, id } = this.$route.params
+    this.animal = this[species][id]
+  }
+}
+</script>
